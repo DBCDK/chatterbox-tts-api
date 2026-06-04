7793640c31d5611a8e0ff4b8f80a308d965623bf
Kristian Nørgaard Jensen <krje@dbc.dk>
2026-06-04 20:30:06 +0200

    Removing previous text processing

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -3,12 +3,6 @@ Core functionality for Chatterbox TTS API
 """
 
 from .memory import get_memory_info, cleanup_memory, safe_delete_tensors
-from .text_processing import (
-    split_text_into_chunks,
-    concatenate_audio_chunks,
-    split_text_for_streaming,
-    get_streaming_settings,
-)
 from .tts_model import initialize_model, get_model
 from .version import get_version, get_version_info
 
@@ -16,10 +10,6 @@ __all__ = [
     "get_memory_info",
     "cleanup_memory",
     "safe_delete_tensors",
-    "split_text_into_chunks",
-    "concatenate_audio_chunks",
-    "split_text_for_streaming",
-    "get_streaming_settings",
     "initialize_model",
     "get_model",
     "get_version",
